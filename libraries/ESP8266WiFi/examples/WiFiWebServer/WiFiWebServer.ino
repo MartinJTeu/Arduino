0205a07b6120898d1dbe6325f252ccfb4d9e1810
MartinJTeu <teufel@cs.uni-frankfurt.de>
2018-04-21 16:21:13 +0200

    Update WiFiWebServer.ino

--- libraries/ESP8266WiFi/examples/WiFiWebServer/WiFiWebServer.ino
+++ libraries/ESP8266WiFi/examples/WiFiWebServer/WiFiWebServer.ino
@@ -1,18 +1,22 @@
-/*
-    This sketch demonstrates how to set up a simple HTTP-like server.
-    The server will set a GPIO pin depending on the request
-      http://server_ip/gpio/0 will set the GPIO2 low,
-      http://server_ip/gpio/1 will set the GPIO2 high
-    server_ip is the IP address of the ESP8266 module, will be
-    printed to Serial when the module is connected.
-*/
+/*  This sketch demonstrates how to set up a simple HTTP-like server.
+ *  The server will output the GPIO state(s) and set a GPIO pin depending on the request.
+ *   http://server_ip/gpio2=0 will set the GPIO2 low,
+ *   http://server_ip/gpio2=1 will set the GPIO2 high
+ *  server_ip is the IP address of the ESP8266 module, will be
+ *  printed to Serial when the module is connected
+ *  hostname will work fine too, of course 
+ */
 
 #include <ESP8266WiFi.h>
+// wifi_set_macaddr() needs "user_interface.h"
+/* extern "C" {
+#include "user_interface.h"
+*/}
 
 const char* ssid = "your-ssid";
 const char* password = "your-password";
 
-// Create an instance of the server
+// create an instance of the server
 // specify the port to listen on as an argument
 WiFiServer server(80);
 
@@ -23,77 +27,88 @@ void setup() {
   // prepare GPIO2
   pinMode(2, OUTPUT);
   digitalWrite(2, 0);
-
-  // Connect to WiFi network
-  Serial.println();
+  
+  // connect to WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
+  // change MAC address (I needed to...)
+//   uint8_t mac[6] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+//   wifi_set_macaddr(STATION_IF, mac);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.println("WiFi connected");
-
+  Serial.print("Connected to "); Serial.println(ssid);
+  
   // Start the server
   server.begin();
   Serial.println("Server started");
 
-  // Print the IP address
+  // print the IP address
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // Check if a client has connected
+  // check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
     return;
   }
-
-  // Wait until the client sends some data
-  Serial.println("new client");
-  while (!client.available()) {
+  
+  // wait until the client sends some data
+  Serial.println("\nNew client connected");
+  while(!client.available()){
     delay(1);
   }
-
-  // Read the first line of the request
+  
+  // read the first line of the request
   String req = client.readStringUntil('\r');
-  Serial.println(req);
-  client.flush();
+  Serial.print("Client request: "); Serial.println(req);
+//  client.flush();
 
-  // Match the request
-  int val;
-  if (req.indexOf("/gpio/0") != -1) {
-    val = 0;
-  } else if (req.indexOf("/gpio/1") != -1) {
-    val = 1;
-  } else {
-    Serial.println("invalid request");
-    client.stop();
-    return;
+  // read current state
+  char state = digitalRead(2);
+  // print to serial
+  Serial.print("GPIO2 state is "); Serial.println((state)?"HIGH":"LOW");
+    
+  // send the current GPIO state back to client
+  String response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n<div>GPIO2 state is ";
+  response += (state)?"HIGH":"LOW";
+  response += "</div>\n";
+  
+  // match the request
+  if (req.indexOf("/gpio2=") != -1){
+    char val;
+    if (req.indexOf("/gpio2=0") != -1) val = 0;
+    else if (req.indexOf("/gpio2=1") != -1) val = 1;
+    else {
+      Serial.println("invalid request");
+      client.stop();
+      return;
+    }
+    // set GPIO2 according to the request
+    digitalWrite(2, val);
+    // print to serial
+    Serial.print("GPIO2 set to "); Serial.println((val)?"HIGH":"LOW");
+    // create response to client
+    response += "<div>GPIO2 set to ";
+    response += (val)?"HIGH":"LOW";
+    response += "</div>\n";
   }
-
-  // Set GPIO2 according to the request
-  digitalWrite(2, val);
-
+  response += "</html>";
+  // send response to the client
+  client.print(response); delay(1);
+  
   client.flush();
+  Serial.println("Client disconnected");
 
-  // Prepare the response
-  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now ";
-  s += (val) ? "high" : "low";
-  s += "</html>\n";
-
-  // Send the response to the client
-  client.print(s);
-  delay(1);
-  Serial.println("Client disonnected");
-
-  // The client will actually be disconnected
-  // when the function returns and 'client' object is detroyed
+  // the client will actually be disconnected 
+  // when the function returns and 'client' object is destroyed
 }
-
